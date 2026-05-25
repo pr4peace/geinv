a7458952e86495d3964bddc64979ba93547c6d6e
Prashanth <prashanth.palanisamy@gmail.com>
2026-05-25 15:42:45 +0530

    fix: resolve ESLint unused expression in NotificationsClient

--- src/components/notifications/NotificationsClient.tsx
+++ src/components/notifications/NotificationsClient.tsx
@@ -65,7 +65,11 @@ function useSection<T extends { id: string }>(items: T[]) {
   function toggle(id: string) {
     setSelected((prev) => {
       const next = new Set(prev)
-      next.has(id) ? next.delete(id) : next.add(id)
+      if (next.has(id)) {
+        next.delete(id)
+      } else {
+        next.add(id)
+      }
       return next
     })
   }
